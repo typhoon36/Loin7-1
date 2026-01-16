0d3aed6d623435c27298d4c55ea2326031a9d653
YEONJUN LEE <154724697+typhoon36@users.noreply.github.com>
2026-01-16 13:42:25 +0900

    unity 0116

---
+++ DragonFlight_Ex/Assets/Scripts/Background.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    //스크롤이 되는 백그라운드
+    public float scrollSpeed = 1f;
+    private Material myMaterial;
+
+    void Start()
+    {
+        //머터리얼 가지고오기
+        myMaterial = GetComponent<Renderer>().material;
+
+
+    }
+
+
+
+    void Update()
+    {
+        //오프셋 머터리얼에서 가져오기
+        Vector2 newOffset = myMaterial.mainTextureOffset;
+        //새롭게 offset바꿔주기
+        //y부분값을 속도에 프레임 보정해서 더해주기
+        newOffset.Set(0, newOffset.y + (scrollSpeed * Time.deltaTime));
+
+        //머터리얼에 오프셋에 값을 넣어준다.
+        myMaterial.mainTextureOffset = newOffset;
+    }
+}
---
+++ DragonFlight_Ex/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //스피드
+    public float moveSpeed = 3;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        //x쪽값 설정 vector 방향 * 시간 * 스피드
+        float distanceX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+
+        //x쪽 이동 설정
+        transform.Translate(distanceX, 0, 0);
+    }
+}
